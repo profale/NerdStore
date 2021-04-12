7c944efae99c69931d205d97052b88f66aa3c7c3
Alessandra Ferreira <profale@gmail.com>
2021-04-11 21:07:42 -0300

    Finalização da implementação dos repositórios.

--- src/NerdStore.Catalogo.Domain/Categoria.cs
+++ src/NerdStore.Catalogo.Domain/Categoria.cs
@@ -18,6 +18,11 @@ namespace NerdStore.Catalogo.Domain
         public string Nome { get; private set; }
         public int Codigo { get; private set; }
 
+        //EF Relation
+        public ICollection<Produto> Produtos { get; set; }
+
+        protected Categoria() { }
+
         public override string ToString()
         {
             return $"{Nome} - {Codigo}";
---
+++ src/NerdStore.Core/Data/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NerdStore.Core.DomainObjects;
+
+namespace NerdStore.Core.Data
+{
+    public interface IRepository<T> : IDisposable where T : IAggregateRoot
+    {
+        IUnitOfWork UnitOfWork { get; }
+    }
+}
